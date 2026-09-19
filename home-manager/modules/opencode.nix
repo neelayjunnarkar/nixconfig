fe4b69a91f25b5c9c35bc125081deed54da88d1c
Neelay <neelay.junnarkar@gmail.com>
2026-09-18 18:01:48 -0700

    add opencode fix

--- home-manager/modules/opencode.nix
+++ home-manager/modules/opencode.nix
@@ -5,7 +5,18 @@
 }: {
   programs.opencode = {
     enable = true;
-    package = pkgs-unstable.opencode;
+    # package = pkgs-unstable.opencode;
+    package = pkgs-unstable.opencode.overrideAttrs (
+      final: prev: {
+        postPatch =
+          prev.postPatch
+          + ''
+            # fix for bun 1.4.x
+            substituteInPlace packages/opencode/script/build.ts \
+              --replace-fail 'splitting: true,' 'splitting: false,'
+          '';
+      }
+    );
     extraPackages = with pkgs; [rust-analyzer];
     settings = {
       lsp = {
